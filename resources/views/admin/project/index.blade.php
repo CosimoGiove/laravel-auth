4d3952ffeb3b24abdf18f03d29e0ea1fb3c49289
cosimogiove <cosimo18497@gmail.com>
2024-02-06 21:47:08 +0100

    feat:portfolio index

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -1,9 +1,35 @@
 @extends('admin.project.layouts.layouts')
-@section("main")
-<header>
-    <div class="text-center">
-        il mio portfolio di progetti.
-    </div>
-</header>
+@section('main')
+    <header>
+        <div class="container">
+            <h1 class="text-center">
+                il mio portfolio di progetti.
+            </h1>
 
+            <ul>
+                @foreach ($newProject as $project)
+                    <li>
+                        <div class="img-utente">
+                            <img src="{{ $project['img'] }}" alt="{{ $project['name'] }}">
+                        </div>
+                        <div>
+                            nome: {{$project["name"]}}
+                        </div>
+                        <div>
+                            cognome: {{$project["surname"]}}
+                        </div>
+                        <div>
+                            età: {{$project["age"]}}
+                        </div>
+                        <div>
+                            frameworks utilizzato: {{$project["frameworks"]}}
+                        </div>
+                        <div>
+                          linguaggio utilizzato  : {{$project["language"]}}
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </header>
 @endsection
