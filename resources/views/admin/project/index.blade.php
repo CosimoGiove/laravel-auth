0c04784e164cdbc1237465fbc23d5109778668a2
cosimogiove <cosimo18497@gmail.com>
2024-02-06 17:27:33 +0100

    feat:inizio portfolio

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    ciao
-</body>
-</html>
+@extends('admin.project.layouts.layouts')
+@section("main")
+<header>
+    <div class="text-center">
+        il mio portfolio di progetti.
+    </div>
+</header>
+
+@endsection
