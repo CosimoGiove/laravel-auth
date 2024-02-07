c5748edf5af58a5ceb1d78f47eea00181b7a6ab2
cosimogiove <cosimo18497@gmail.com>
2024-02-07 17:09:33 +0100

    feat: inizio  rappresentazione welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\Home;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[Home::class,"index"]);
 
 
 // Route::get('/dashboard',[DashboardController::class,"index"]
